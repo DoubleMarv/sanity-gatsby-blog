ea14f1e1b65c39a983b56d07e6f37e441a69d254
BarrytronCharlies <info@doublemarvellous.com>
2021-05-19 12:54:25 +0100

    Try again
    initial class component failer

--- web/src/components/banner.js
+++ web/src/components/banner.js
@@ -1,12 +1,13 @@
 import * as styles from "./banner.module.css";
-import React, { Component } from 'react'
+import React from "react";
 
-export default class Banner extends Component(props) {
-    render() {
-        return (
-            <div className={styles.mainBanner}>
-                <p>This is a banner</p>
-            </div>
-        )
-    }
+
+function Banner() {
+  return (
+    <div className={styles.mainBanner}>
+    <p>This is a banner</p>
+</div>
+  );
 }
+
+export default Banner;
